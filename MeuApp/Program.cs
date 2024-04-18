32a1d2f0489c13c4be8a42cc38863ec4da295532
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-04-18 10:12:52 -0300

    poo

--- MeuApp/Program.cs
+++ MeuApp/Program.cs
@@ -3,20 +3,9 @@ using System.Runtime.InteropServices;
 
 namespace MeuApp
 {
+ 
     class Program {
         static void Main() {
-
-
-        }
-
-        class Pag() {
-            public DateTime vencimento { get; set; }
-            public DateTime pagamento { get; set; }
-
-            public Pag(DateTime vencimento, DateTime pagamento){
-                this.vencimento = vencimento;
-                this.pagamento = pagamento;
-            }
         }
     }
 }
